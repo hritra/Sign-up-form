708e5c8358be0e511fe98142e2710ae957042532
hritra <harrytagotra@gmail.com>
2023-05-15 19:54:29 +0530

    minor changes

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ function checkPassword(){
     if(password.length != 0){
         if(password == cnfrmPassword){
             message.textContent = "Passwords Match";
+            message.style.backgroundColor = "#3ae374";
         }
         else{
             message.textContent = "Passwords do not match";
+            message.style.backgroundColor = "#ff4d4d";
         }
     }
 }
